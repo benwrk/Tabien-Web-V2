0643b59562aed06c7d2a98a8e30600eb1f2f0a2f
Benjapol Worakan <benwrk@live.com>
2016-04-27 16:16:57 +0700

    Login and Sign Up page completed.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,35 @@
+"use_strict";
+
 var app = angular.module('chirpApp', []);
 
 app.controller('chirpController', function ($scope) {
     $scope.posts = [];
     $scope.newPost = {
-        created_by: "",
-        text: "",
-        created_time: ""
+        created_by: '',
+        text: '',
+        created_time: ''
     };
 
     $scope.post = function () {
         $scope.newPost.created_time = Date.now();
         $scope.posts.push($scope.newPost);
         $scope.newPost = { created_by: "", text: "", created_time: "" };
+    };
+});
+
+app.controller('authController', function ($scope) {
+    $scope.user = { username: '', password: '' };
+    $scope.error_message = '';
+
+    // postService.getAll().success(function (data) {
+    //     $scope.posts = data;
+    // });
+
+    $scope.login = function () {
+        $scope.error_message = 'login request for ' + $scope.user.username;
+    };
+
+    $scope.signup = function () {
+        $scope.error_message = 'signup request for ' + $scope.user.username;
     };
 });
